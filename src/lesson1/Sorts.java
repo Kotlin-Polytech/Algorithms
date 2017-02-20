1ce12e4b42d94d57f594eeb0c790c7dcc9a13536
mglukhikh <glukhikh@mail.ru>
2017-02-20 12:40:22 +0300

    Heap sort added

--- src/lesson1/Sorts.java
+++ src/lesson1/Sorts.java
@@ -56,4 +56,38 @@ public class Sorts {
     public static void mergeSort(int[] elements) {
         mergeSort(elements, 0, elements.length);
     }
+
+    private static void heapify(int[] elements, int start, int length) {
+        int left = 2 * start + 1;
+        int right = left + 1;
+        int max = start;
+        if (left < length && elements[left] > elements[max]) {
+            max = left;
+        }
+        if (right < length && elements[right] > elements[max]) {
+            max = right;
+        }
+        if (max != start) {
+            int temp = elements[max];
+            elements[max] = elements[start];
+            elements[start] = temp;
+            heapify(elements, max, length);
+        }
+    }
+
+    private static void buildHeap(int[] elements) {
+        for (int start = elements.length / 2 - 1; start >= 0; start--) {
+            heapify(elements, start, elements.length);
+        }
+    }
+
+    public static void heapSort(int[] elements) {
+        buildHeap(elements);
+        for (int j = elements.length - 1; j >= 1; j--) {
+            int temp = elements[0];
+            elements[0] = elements[j];
+            elements[j] = temp;
+            heapify(elements, 0, j);
+        }
+    }
 }
