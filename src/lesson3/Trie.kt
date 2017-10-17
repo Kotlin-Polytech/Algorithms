92a9e9976d5b56c2b8cf7971d24ad1bf77179e60
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-10-18 00:00:40 +0300

    Add primitive String trie implementation

---
+++ src/lesson3/Trie.kt
@@ -0,0 +1,64 @@
+package lesson3
+
+class Trie : AbstractMutableSet<String>(), MutableSet<String> {
+    override var size: Int = 0
+        private set
+
+    private class Node {
+        val children: MutableMap<Char, Node> = linkedMapOf()
+    }
+
+    private var root = Node()
+
+    override fun clear() {
+        root.children.clear()
+        size = 0
+    }
+
+    private fun String.withZero() = this + 0.toChar()
+
+    private fun findNode(element: String): Node? {
+        var current = root
+        for (char in element) {
+            current = current.children[char] ?: return null
+        }
+        return current
+    }
+
+    override fun contains(element: String): Boolean =
+            findNode(element.withZero()) != null
+
+    override fun add(element: String): Boolean {
+        var current = root
+        var modified = false
+        for (char in element.withZero()) {
+            val child = current.children[char]
+            if (child != null) {
+                current = child
+            }
+            else {
+                modified = true
+                val newChild = Node()
+                current.children[char] = newChild
+                current = newChild
+            }
+        }
+        if (modified) {
+            size++
+        }
+        return modified
+    }
+
+    override fun remove(element: String): Boolean {
+        val current = findNode(element) ?: return false
+        if (current.children.remove(0.toChar()) != null) {
+            size--
+            return true
+        }
+        return false
+    }
+
+    override fun iterator(): MutableIterator<String> {
+        TODO("not implemented")
+    }
+}
