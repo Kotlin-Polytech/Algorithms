e46e4e872a0bc71a462c64c7681455cee5b94ad7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-10-18 00:00:40 +0300

    Add primitive String trie implementation

---
+++ test/lesson3/TrieTest.kt
@@ -0,0 +1,42 @@
+package lesson3
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TrieTest {
+
+    @Test
+    fun generalTest() {
+        val trie = Trie()
+        trie.add("abcdefg")
+        assertTrue("abcdefg" in trie)
+        assertFalse("abcdef" in trie)
+        assertFalse("a" in trie)
+        assertFalse("g" in trie)
+
+        trie.add("zyx")
+        trie.add("zwv")
+        trie.add("zyt")
+        trie.add("abcde")
+        assertEquals(5, trie.size)
+        assertTrue("abcdefg" in trie)
+        assertFalse("abcdef" in trie)
+        assertTrue("abcde" in trie)
+        assertTrue("zyx" in trie)
+        assertTrue("zyt" in trie)
+        assertTrue("zwv" in trie)
+        assertFalse("zy" in trie)
+        assertFalse("zv" in trie)
+
+        trie.remove("zwv")
+        trie.remove("zy")
+        assertEquals(4, trie.size)
+        assertTrue("zyt" in trie)
+        assertFalse("zwv" in trie)
+
+        trie.clear()
+        assertEquals(0, trie.size)
+        assertFalse("zyx" in trie)
+    }
+}
