7831feb11b63fb9c2ae9d0ce411a7df7192bc8eb
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-10-17 17:23:57 +0300

    Add kotlin version of BinaryTree, kotlinize its tests

---
+++ test/lesson3/BinaryTreeTest.kt
@@ -0,0 +1,59 @@
+package lesson3
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BinaryTreeTest {
+    @Test
+    fun add() {
+        val tree = BinaryTree<Int>()
+        tree.add(10)
+        tree.add(5)
+        tree.add(7)
+        tree.add(10)
+        assertEquals(3, tree.size)
+        assertTrue(tree.contains(5))
+        tree.add(3)
+        tree.add(1)
+        tree.add(3)
+        tree.add(4)
+        assertEquals(6, tree.size)
+        assertFalse(tree.contains(8))
+        tree.add(8)
+        tree.add(15)
+        tree.add(15)
+        tree.add(20)
+        assertEquals(9, tree.size)
+        assertTrue(tree.contains(8))
+        assertTrue(tree.checkInvariant())
+        assertEquals(1, tree.first())
+        assertEquals(20, tree.last())
+    }
+
+    @Test
+    fun addKotlin() {
+        val tree = KtBinaryTree<Int>()
+        tree.add(10)
+        tree.add(5)
+        tree.add(7)
+        tree.add(10)
+        assertEquals(3, tree.size)
+        assertTrue(tree.contains(5))
+        tree.add(3)
+        tree.add(1)
+        tree.add(3)
+        tree.add(4)
+        assertEquals(6, tree.size)
+        assertFalse(tree.contains(8))
+        tree.add(8)
+        tree.add(15)
+        tree.add(15)
+        tree.add(20)
+        assertEquals(9, tree.size)
+        assertTrue(tree.contains(8))
+        assertTrue(tree.checkInvariant())
+        assertEquals(1, tree.first())
+        assertEquals(20, tree.last())
+    }
+}
