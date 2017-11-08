c644a6bbcf806d9e36f1253779670af68d099c9b
Malik <hiraev@users.noreply.github.com>
2017-11-09 02:48:52 +0300

    Тесты, тесты

--- test/lesson3/BinaryTreeStudentTest.java
+++ test/lesson3/BinaryTreeStudentTest.java
@@ -1,30 +1,27 @@
 package lesson3;
 
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.junit.Assert.*;
-
 import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-
 public class BinaryTreeStudentTest {
-    BinaryTree<Integer> binaryTree = new BinaryTree<>();
-    List<Integer> list = Arrays.asList(7,4,2,6,5,1,8,5,2,7,10,18,72);
+    private BinaryTree<Integer> binaryTree = new BinaryTree<>();
+    private List<Integer> list = Arrays.asList(7, 4, 2, 6, 5, 1, 8, 10, 18, 72);
+    private List<Integer> testList = new ArrayList<>();
 
     @Before
-    public void filling() {
-        //Collections.shuffle(list);
+    public void preparation() {
+//        Collections.shuffle(list);
         binaryTree.addAll(list);
+        Collections.sort(list);
     }
 
-    @Test()
+    @Test
     public void testIterator() {
-        Iterator<Integer> iterator = binaryTree.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-            System.out.println("------------------------");
+        binaryTree.iterator().forEachRemaining(testList::add);
+        Collections.sort(testList);
+        assertArrayEquals(list.toArray(), testList.toArray());
     }
 }
