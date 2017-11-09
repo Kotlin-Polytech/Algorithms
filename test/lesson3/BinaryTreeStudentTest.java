85681023e69e714c7db411caccfa3aa159d2c5fa
Malik <hiraev@users.noreply.github.com>
2017-11-09 14:42:22 +0300

    Добавлены тесты для метода remove(). Рассмотрены все случаи (удаление листа, узла с одним и дочерним элементом, а так же удаление случайного узла в случайном дереве)

--- test/lesson3/BinaryTreeStudentTest.java
+++ test/lesson3/BinaryTreeStudentTest.java
@@ -1,10 +1,11 @@
 package lesson3;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class BinaryTreeStudentTest {
     private BinaryTree<Integer> binaryTree = new BinaryTree<>();
@@ -13,15 +14,80 @@ public class BinaryTreeStudentTest {
 
     @Before
     public void preparation() {
-//        Collections.shuffle(list);
-        binaryTree.addAll(list);
-        Collections.sort(list);
+        testList.clear();
     }
 
     @Test
     public void testIterator() {
+        Collections.shuffle(list);
+        binaryTree.addAll(list);
         binaryTree.iterator().forEachRemaining(testList::add);
-        Collections.sort(testList);
+        sortLists();
         assertArrayEquals(list.toArray(), testList.toArray());
+    }
+
+    @Test
+    public void removeLeaf() {
+        binaryTree.addAll(list);
+        Iterator<Integer> iterator = binaryTree.iterator();
+        while (iterator.hasNext()) {
+            int it = iterator.next();
+            if (it == 1) iterator.remove();
+            testList.add(it);
+        }
+        sortLists();
+        assertArrayEquals(testList.toArray(), list.toArray());
+        assertFalse(binaryTree.contains(1));
+        assertTrue(binaryTree.checkInvariant());
+    }
+
+    @Test
+    public void removeNodeWithOneChild() {
+        binaryTree.addAll(list);
+        Iterator<Integer> iterator = binaryTree.iterator();
+        while (iterator.hasNext()) {
+            int it = iterator.next();
+            if (it == 8) iterator.remove();
+            testList.add(it);
+        }
+        sortLists();
+        assertArrayEquals(testList.toArray(), list.toArray());
+        assertFalse(binaryTree.contains(8));
+        assertTrue(binaryTree.checkInvariant());
+    }
+
+    @Test
+    public void removeNodeWithTwoChildren() {
+        binaryTree.addAll(list);
+        int sizeBeforeRemoving = binaryTree.size();
+        Iterator<Integer> iterator = binaryTree.iterator();
+        while (iterator.hasNext()) {
+            int it = iterator.next();
+            if (it == 7) iterator.remove();
+        }
+        assertFalse(binaryTree.contains(7));
+        assertTrue(binaryTree.checkInvariant());
+        assertTrue(binaryTree.size() == sizeBeforeRemoving - 1);
+    }
+
+    @Test
+    public void removeInRandomTree() {
+        Collections.shuffle(list);
+        binaryTree.addAll(list);
+        int sizeBeforeRemoving = binaryTree.size();
+        Iterator<Integer> iterator = binaryTree.iterator();
+        int randomIndex = new Random().nextInt(list.size() - 1);
+        while (iterator.hasNext()) {
+            int it = iterator.next();
+            if (it == list.get(randomIndex)) iterator.remove();
+        }
+        assertFalse(binaryTree.contains(list.get(randomIndex)));
+        assertTrue(binaryTree.checkInvariant());
+        assertTrue(binaryTree.size() == sizeBeforeRemoving - 1);
+    }
+
+    private void sortLists() {
+        Collections.sort(testList);
+        Collections.sort(list);
     }
 }
