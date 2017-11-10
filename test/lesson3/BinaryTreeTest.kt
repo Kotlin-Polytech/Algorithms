c32041234fa885c1953672aacd30252a4b920b70
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-11-10 13:59:56 +0300

    Test for BinaryTree.iterator()

--- test/lesson3/BinaryTreeTest.kt
+++ test/lesson3/BinaryTreeTest.kt
@@ -99,6 +99,38 @@ class BinaryTreeTest {
         testRemove { createJavaTree() }
     }
 
+    private fun testIterator(create: () -> CheckableSortedSet<Int>) {
+        val random = Random()
+        for (iteration in 1..100) {
+            val list = mutableListOf<Int>()
+            for (i in 1..20) {
+                list.add(random.nextInt(100))
+            }
+            val treeSet = TreeSet<Int>()
+            val binarySet = create()
+            for (element in list) {
+                treeSet += element
+                binarySet += element
+            }
+            val treeIt = treeSet.iterator()
+            val binaryIt = binarySet.iterator()
+            println("Traversing $list")
+            while (treeIt.hasNext()) {
+                assertEquals(treeIt.next(), binaryIt.next())
+            }
+        }
+    }
+
+    @Test
+    fun testIteratorKotlin() {
+        testIterator { createKotlinTree() }
+    }
+
+    @Test
+    fun testIteratorJava() {
+        testIterator { createJavaTree() }
+    }
+
     private fun testIteratorRemove(create: () -> CheckableSortedSet<Int>) {
         val random = Random()
         for (iteration in 1..100) {
