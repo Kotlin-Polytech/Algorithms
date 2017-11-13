bb91bf9fbbbb21bf36649be58d99c7b89bfd78f1
Malik <hiraev@users.noreply.github.com>
2017-11-14 00:29:43 +0300

    Добавлены новые тесты для проверки всех возможных случаев

--- test/lesson3/BinaryTreeStudentTest.java
+++ test/lesson3/BinaryTreeStudentTest.java
@@ -71,18 +71,88 @@ public class BinaryTreeStudentTest {
     }
 
     @Test
-    public void removeInRandomTree() {
-        Collections.shuffle(list);
+    public void testByMikhailIgorevich() {
+        List<Integer> list = Arrays.asList(26, 41, 31, 29, 28, 86, 87, 37, 97, 70, 97, 6, 57, 98, 35, 95, 43, 70, 48, 62);
         binaryTree.addAll(list);
-        int sizeBeforeRemoving = binaryTree.size();
+        Collections.sort(list);
+        binaryTree.remove(31);
         Iterator<Integer> iterator = binaryTree.iterator();
-        int randomIndex = new Random().nextInt(list.size() - 1);
+        assertTrue(binaryTree.checkInvariant());
+        assertFalse(binaryTree.contains(31));
         while (iterator.hasNext()) {
-            int it = iterator.next();
-            if (it == list.get(randomIndex)) iterator.remove();
+            iterator.next();
+        }
+    }
+
+    @Test
+    public void removeInRandomTreeByIterator() {
+        for (int i = 0; i < 1000; i++) {
+            assertTrue(forRemoveInRandomTreeByIterator());
+        }
+    }
+
+    private boolean forRemoveInRandomTreeByIterator() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        while (binaryTree.size() != 10) {
+            int next = random.nextInt(1000);
+            if (binaryTree.add(next)) list.add(next);
+        }
+
+        Iterator<Integer> iterator = binaryTree.iterator();
+        int randomIndex = random.nextInt(10);
+        try {
+            while (iterator.hasNext()) {
+                int it = iterator.next();
+                if (it == list.get(randomIndex)) iterator.remove();
+            }
+        } catch (NullPointerException e) {
+            System.out.println(list);
+            System.out.println("Пытались удалить: " + list.get(randomIndex));
+            e.printStackTrace();
+            return false;
         }
         assertFalse(binaryTree.contains(list.get(randomIndex)));
         assertTrue(binaryTree.checkInvariant());
-        assertTrue(binaryTree.size() == sizeBeforeRemoving - 1);
+        assertTrue(binaryTree.size() == 9);
+        return true;
+    }
+
+    @Test
+    public void removeInRandomTree() {
+        for (int i = 0; i < 1000; i++) {
+            assertTrue(forRemoveInRandomTree());
+        }
+    }
+
+    private boolean forRemoveInRandomTree() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        //Заполняем дерево 50 случайными числами от 0 до 1000
+        while (binaryTree.size() != 10) {
+            int next = random.nextInt(1000);
+            if (binaryTree.add(next)) list.add(next);
+        }
+        //Удаляем из дерева случайное число
+        int removeIt = list.get(random.nextInt(10));
+        assertTrue(binaryTree.remove(removeIt));
+        Iterator<Integer> iterator = binaryTree.iterator();
+        int x = -1;
+        try {
+            while (iterator.hasNext()) {
+                x = iterator.next();
+            }
+        } catch (Exception e) {
+            System.out.println(list);
+            System.out.println("Последний элемент до ошибки: " + x);
+            System.out.println("Пытались удалить: " + removeIt);
+            e.printStackTrace();
+            return false;
+        }
+        assertTrue(binaryTree.checkInvariant());
+        return true;
     }
 }
