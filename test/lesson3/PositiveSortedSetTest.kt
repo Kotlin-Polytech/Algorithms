8670e5ec4b11f17331e8f3c09d698038ec8e31c9
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-11-14 19:55:58 +0300

    PositiveSortedSet

---
+++ test/lesson3/PositiveSortedSetTest.kt
@@ -0,0 +1,33 @@
+package lesson3
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.TreeSet
+
+class PositiveSortedSetTest {
+    @Test
+    fun basic() {
+        val set = listOf(7, -4, 5, 9, -3, 4, 0, 99, -12).let {
+            val tree = TreeSet<Int>()
+            tree.addAll(it)
+            tree
+        }
+        val subSet = set.positives()
+        assertEquals(5, subSet.size)
+        subSet.add(10)
+        assertEquals(6, subSet.size)
+        assertEquals(10, set.size)
+        subSet.add(-10)
+        assertEquals(6, subSet.size)
+        assertEquals(10, set.size)
+        set.remove(5)
+        assertEquals(5, subSet.size)
+        assertEquals(9, set.size)
+        set.remove(-4)
+        assertEquals(5, subSet.size)
+        assertEquals(8, set.size)
+        subSet.remove(99)
+        assertEquals(4, subSet.size)
+        assertEquals(7, set.size)
+    }
+}
