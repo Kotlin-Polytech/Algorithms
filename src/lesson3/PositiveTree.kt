bef52ce5a5a06a99cbc320331c5ea5296ac8b635
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-11-14 19:55:58 +0300

    PositiveSortedSet

---
+++ src/lesson3/PositiveTree.kt
@@ -0,0 +1,75 @@
+package lesson3
+
+import java.util.Comparator
+import java.util.SortedSet
+
+class PositiveSortedSet(private val delegate: SortedSet<Int>) : AbstractMutableSet<Int>(), SortedSet<Int> {
+    override fun comparator(): Comparator<in Int>? = delegate.comparator()
+
+    override fun subSet(fromElement: Int?, toElement: Int?): SortedSet<Int> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun headSet(toElement: Int?): SortedSet<Int> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun tailSet(fromElement: Int?): SortedSet<Int> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun last(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun first(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun add(element: Int): Boolean {
+        if (element <= 0) return false
+        return delegate.add(element)
+    }
+
+    override fun remove(element: Int): Boolean {
+        if (element <= 0) return false
+        return delegate.remove(element)
+    }
+
+    override fun iterator(): MutableIterator<Int> = object : MutableIterator<Int> {
+        private val delegate = this@PositiveSortedSet.delegate.iterator()
+
+        private var next: Int? = null
+
+        init {
+            while (delegate.hasNext()) {
+                val next = delegate.next()
+                if (next > 0) {
+                    this.next = next
+                    break
+                }
+            }
+        }
+
+        override fun hasNext(): Boolean {
+            return next != null
+        }
+
+        override fun next(): Int {
+            val result = next ?: throw NoSuchElementException()
+            next = if (delegate.hasNext()) delegate.next() else null
+            return result
+        }
+
+        override fun remove() {
+            delegate.remove()
+        }
+
+    }
+
+    override val size: Int
+        get() = delegate.count { it > 0 }
+
+}
+
+fun SortedSet<Int>.positives(): SortedSet<Int> = PositiveSortedSet(this)
