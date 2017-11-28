71934e7dc19a778e3a35c10141d7eb539efbaa12
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-11-28 23:27:32 +0300

    Long test for genetic voyaging searcher

--- test/lesson5/genetic/GeneticVoyagingPathSearcherTest.kt
+++ test/lesson5/genetic/GeneticVoyagingPathSearcherTest.kt
@@ -1,9 +1,11 @@
 package lesson5.genetic
 
+import lesson2.Graph
 import lesson2.impl.GraphBuilder
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 class GeneticVoyagingPathSearcherTest {
     @Test
@@ -29,6 +31,25 @@ class GeneticVoyagingPathSearcherTest {
         assertEquals(105, path.length)
         assertEquals(listOf(graph["A"], graph["D"], graph["B"], graph["C"], graph["E"], graph["F"], graph["A"]),
                 path.vertices)
+    }
+
+    @Test
+    fun findRandomVoyagingPath() {
+        val random = Random()
+        val graph = GraphBuilder().apply {
+            val vertices = mutableListOf<Graph.Vertex>()
+            for (i in 0..99) {
+                vertices += addVertex(i.toString())
+            }
+            for (i in 0..99) {
+                for (j in i + 1..99) {
+                    addConnection(vertices[i], vertices[j], 1 + random.nextInt(100))
+                }
+            }
+        }.build()
+        val path = GeneticVoyagingPathSearcher(graph).findVoyagingPath(chromosomeNumber = 100, generationNumber = 200)
+        println(path.length)
+        println(path)
     }
 
 }
