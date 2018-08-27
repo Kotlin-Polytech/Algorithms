3c4fb6f437fc7f15d1986ea191fab52cf188b5fa
Mikhail Glukhikh <glukhikh@mail.ru>
2018-08-27 11:52:37 +0300

    Additional tests for times task

--- test/lesson1/TaskTests.kt
+++ test/lesson1/TaskTests.kt
@@ -9,7 +9,7 @@ class TaskTests {
     private fun assertFileContent(name: String, expectedContent: String) {
         val file = File(name)
         val content = file.readLines().joinToString("\n")
-        assertEquals(expectedContent, content)
+        assertEquals(expectedContent.trim(), content.trim())
     }
 
     @Test
@@ -25,6 +25,16 @@ class TaskTests {
                      19:56:14
                 """.trimIndent()
         )
+        File("temp.txt").delete()
+        sortTimes("input/time_in2.txt", "temp.txt")
+        assertFileContent("temp.txt",
+                """
+                     00:00:00
+                """.trimIndent()
+        )
+        File("temp.txt").delete()
+        sortTimes("input/time_in3.txt", "temp.txt")
+        assertFileContent("temp.txt", File("input/time_out3.txt").readLines().joinToString(separator = "\n"))
         File("temp.txt").delete()
     }
 }
