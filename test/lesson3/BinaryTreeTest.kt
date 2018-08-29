0a9443ea863c0ef6beb15fc3a0d08c1f5177bd58
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-08-29 12:02:12 +0300

    Refactor test for BinaryTree.add

--- test/lesson3/BinaryTreeTest.kt
+++ test/lesson3/BinaryTreeTest.kt
@@ -6,9 +6,8 @@ import org.junit.Assert.*
 import java.util.*
 
 class BinaryTreeTest {
-    @Test
-    fun add() {
-        val tree = BinaryTree<Int>()
+    private fun testAdd(create: () -> CheckableSortedSet<Int>) {
+        val tree = create()
         tree.add(10)
         tree.add(5)
         tree.add(7)
@@ -33,29 +32,13 @@ class BinaryTreeTest {
     }
 
     @Test
-    fun addKotlin() {
-        val tree = KtBinaryTree<Int>()
-        tree.add(10)
-        tree.add(5)
-        tree.add(7)
-        tree.add(10)
-        assertEquals(3, tree.size)
-        assertTrue(tree.contains(5))
-        tree.add(3)
-        tree.add(1)
-        tree.add(3)
-        tree.add(4)
-        assertEquals(6, tree.size)
-        assertFalse(tree.contains(8))
-        tree.add(8)
-        tree.add(15)
-        tree.add(15)
-        tree.add(20)
-        assertEquals(9, tree.size)
-        assertTrue(tree.contains(8))
-        assertTrue(tree.checkInvariant())
-        assertEquals(1, tree.first())
-        assertEquals(20, tree.last())
+    fun testAddKotlin() {
+        testAdd { createKotlinTree() }
+    }
+
+    @Test
+    fun testAddJava() {
+        testAdd { createJavaTree() }
     }
 
     private fun <T : Comparable<T>> createJavaTree(): CheckableSortedSet<T> = BinaryTree()
