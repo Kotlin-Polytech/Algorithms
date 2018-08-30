058481ddae0434a1f39e17b7b7d53fd42fa718e2
Mikhail Glukhikh <glukhikh@mail.ru>
2018-08-30 12:16:17 +0300

    Add sortTemperatures task

--- test/lesson1/TaskTests.kt
+++ test/lesson1/TaskTests.kt
@@ -1,6 +1,9 @@
 package lesson1
 
+import java.io.BufferedWriter
 import java.io.File
+import java.util.*
+import kotlin.math.abs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -14,9 +17,10 @@ class TaskTests {
 
     @Test
     fun sortTimes() {
-        sortTimes("input/time_in1.txt", "temp.txt")
-        assertFileContent("temp.txt",
-                """
+        try {
+            sortTimes("input/time_in1.txt", "temp.txt")
+            assertFileContent("temp.txt",
+                    """
                      00:40:31
                      07:26:57
                      10:00:03
@@ -24,17 +28,85 @@ class TaskTests {
                      13:15:19
                      19:56:14
                 """.trimIndent()
-        )
-        File("temp.txt").delete()
-        sortTimes("input/time_in2.txt", "temp.txt")
-        assertFileContent("temp.txt",
-                """
+            )
+        } finally {
+            File("temp.txt").delete()
+        }
+        try {
+            sortTimes("input/time_in2.txt", "temp.txt")
+            assertFileContent("temp.txt",
+                    """
                      00:00:00
                 """.trimIndent()
-        )
-        File("temp.txt").delete()
-        sortTimes("input/time_in3.txt", "temp.txt")
-        assertFileContent("temp.txt", File("input/time_out3.txt").readLines().joinToString(separator = "\n"))
-        File("temp.txt").delete()
+            )
+        } finally {
+            File("temp.txt").delete()
+        }
+        try {
+            sortTimes("input/time_in3.txt", "temp.txt")
+            assertFileContent("temp.txt", File("input/time_out3.txt").readLines().joinToString(separator = "\n"))
+        } finally {
+            File("temp.txt").delete()
+        }
+    }
+
+    private fun generateTemperatures(size: Int) {
+        val random = Random()
+        val temperatures = mutableListOf<Int>()
+        for (t in -2730..5000) {
+            val count = random.nextInt(size)
+            for (i in 1..count) {
+                temperatures += t
+            }
+        }
+
+        fun BufferedWriter.writeTemperatures() {
+            for (t in temperatures) {
+                if (t < 0) write("-")
+                write("${abs(t) / 10}.${abs(t) % 10}")
+                newLine()
+            }
+            close()
+        }
+
+        File("temp_sorted_expected.txt").bufferedWriter().writeTemperatures()
+        temperatures.shuffle(random)
+        File("temp_unsorted.txt").bufferedWriter().writeTemperatures()
+    }
+
+    @Test
+    fun sortTemperatures() {
+        try {
+            sortTemperatures("input/temp_in1.txt", "temp.txt")
+            assertFileContent("temp.txt",
+                    """
+                    -98.4
+                    -12.6
+                    -12.6
+                    11.0
+                    24.7
+                    99.5
+                    121.3
+                """.trimIndent()
+            )
+        } finally {
+            File("temp.txt").delete()
+        }
+
+        fun testGeneratedTemperatures(size: Int) {
+            try {
+                generateTemperatures(size)
+                sortTemperatures("temp_unsorted.txt", "temp_sorted_actual.txt")
+                assertFileContent("temp_sorted_actual.txt",
+                        File("temp_sorted_expected.txt").readLines().joinToString(separator = "\n")
+                )
+            } finally {
+                File("temp_unsorted.txt").delete()
+                File("temp_sorted_expected.txt").delete()
+                File("temp_sorted_actual.txt").delete()
+            }
+        }
+        testGeneratedTemperatures(10)
+        testGeneratedTemperatures(5000)
     }
 }
