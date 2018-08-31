0779f828d80efd91f9c1adbde22a2707f90bfbaf
Mikhail Glukhikh <glukhikh@mail.ru>
2018-08-31 14:29:54 +0300

    Additional tests for optimizeBuyAndSell

--- test/lesson2/AlgorithmsTests.kt
+++ test/lesson2/AlgorithmsTests.kt
@@ -1,14 +1,65 @@
 package lesson2
 
+import java.io.BufferedWriter
+import java.io.File
+import java.util.*
+import kotlin.math.abs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AlgorithmsTests {
+
+    private val minPrice = 42
+
+    private val maxPrice = 99999
+
+    private fun generatePrices(size: Int): Pair<Int, Int> {
+        val random = Random()
+        val prices = mutableListOf<Int>()
+        for (index in 1..size) {
+            val price = minPrice + 1 + random.nextInt(maxPrice - 1 - minPrice)
+            prices += price
+        }
+        val firstIndex = random.nextInt(size)
+        val secondIndex = random.nextInt(size).let {
+            when (it) {
+                firstIndex -> if (firstIndex == size - 1) firstIndex - 1 else firstIndex + 1
+                else -> it
+            }
+        }
+        val (minIndex, maxIndex) =
+                if (firstIndex < secondIndex) firstIndex to secondIndex else secondIndex to firstIndex
+        prices[minIndex] = minPrice
+        prices[maxIndex] = maxPrice
+
+        fun BufferedWriter.writePrices() {
+            for (price in prices) {
+                write(price.toString())
+                newLine()
+            }
+            close()
+        }
+
+        File("temp_prices.txt").bufferedWriter().writePrices()
+        return minIndex + 1 to maxIndex + 1
+    }
 
     @Test
     fun optimizeBuyAndSell() {
         assertEquals(3 to 4, optimizeBuyAndSell("input/buysell_in1.txt"))
         assertEquals(8 to 12, optimizeBuyAndSell("input/buysell_in2.txt"))
         assertEquals(3 to 4, optimizeBuyAndSell("input/buysell_in3.txt"))
+        try {
+            val expectedAnswer = generatePrices(1000)
+            assertEquals(expectedAnswer, optimizeBuyAndSell("temp_prices.txt"))
+        } finally {
+            File("temp_prices.txt").delete()
+        }
+        try {
+            val expectedAnswer = generatePrices(100000)
+            assertEquals(expectedAnswer, optimizeBuyAndSell("temp_prices.txt"))
+        } finally {
+            File("temp_prices.txt").delete()
+        }
     }
 }
