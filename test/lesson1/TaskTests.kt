5f3cfb86b78f61971bca76dc5bd34cf108ad875c
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-09-03 13:19:48 +0300

    More complicated mergeArrays() tests

--- test/lesson1/TaskTests.kt
+++ test/lesson1/TaskTests.kt
@@ -144,5 +144,35 @@ class TaskTests : AbstractFileTests() {
         val result = arrayOf(null, null, null, null, null, 1, 3, 9, 13, 18, 23)
         mergeArrays(arrayOf(4, 9, 15, 20, 23), result)
         assertArrayEquals(arrayOf(1, 3, 4, 9, 9, 13, 15, 18, 20, 23, 23), result)
+
+        fun generateArrays(firstSize: Int, secondSize: Int): Triple<Array<Int>, Array<Int?>, Array<Int?>> {
+            val random = Random()
+            val expectedResult = Array<Int?>(firstSize + secondSize) {
+                it * 10 + random.nextInt(10)
+            }
+            val first = mutableListOf<Int>()
+            val second = mutableListOf<Int?>()
+            for (i in 1..firstSize) second.add(null)
+            for (element in expectedResult) {
+                if (first.size < firstSize && (random.nextBoolean() || second.size == firstSize + secondSize)) {
+                    first += element!!
+                } else {
+                    second += element
+                }
+            }
+            return Triple(first.toTypedArray(), second.toTypedArray(), expectedResult)
+        }
+
+        run {
+            val (first, second, expectedResult) = generateArrays(20000, 20000)
+            mergeArrays(first, second)
+            assertArrayEquals(expectedResult, second)
+        }
+
+        run {
+            val (first, second, expectedResult) = generateArrays(500000, 500000)
+            mergeArrays(first, second)
+            assertArrayEquals(expectedResult, second)
+        }
     }
 }
