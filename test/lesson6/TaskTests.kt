91c96699a99d9505752bd00a7130a68a21b84bc6
Mikhail Glukhikh <glukhikh@mail.ru>
2018-09-15 11:41:45 +0300

    Lesson 6: add task about longest increasing sub-sequence

--- test/lesson6/TaskTests.kt
+++ test/lesson6/TaskTests.kt
@@ -12,4 +12,16 @@ class TaskTests {
         assertEquals("emt ole", longestCommonSubSequence("nematode knowledge", "empty bottle"))
     }
 
+    @Test
+    fun longestIncreasingSubSequence() {
+        assertEquals(listOf(), longestIncreasingSubSequence(listOf()))
+        assertEquals(listOf(1), longestIncreasingSubSequence(listOf(1)))
+        assertEquals(listOf(1, 2), longestIncreasingSubSequence(listOf(1, 2)))
+        assertEquals(listOf(2, 1), longestIncreasingSubSequence(listOf(2)))
+        assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                longestIncreasingSubSequence(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+        )
+        assertEquals(listOf(2, 8, 9, 12), longestIncreasingSubSequence(listOf(2, 8, 5, 9, 12, 6)))
+    }
+
 }
