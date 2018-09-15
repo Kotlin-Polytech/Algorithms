b6774c67b1bc20489b6b298fea4403b0349d90a9
Mikhail Glukhikh <glukhikh@mail.ru>
2018-09-15 12:55:55 +0300

    Lesson 4: add stub example & test

---
+++ test/lesson4/TableTests.kt
@@ -0,0 +1,12 @@
+package lesson4
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+// TODO: здесь будут тесты для задач и примеров 4-го урока
+class TableTests {
+    @Test
+    fun testSomeExample() {
+        assertEquals(42, someExample())
+    }
+}
