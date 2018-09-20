f1c67c505d55e12d903b89fbed5d8b27d5e93bea
Mikhail Glukhikh <glukhikh@mail.ru>
2018-09-20 09:51:06 +0300

    Lesson 2: set difficulties

--- test/lesson2/AlgorithmsTestsJava.kt
+++ test/lesson2/AlgorithmsTestsJava.kt
@@ -1,29 +1,35 @@
 package lesson2
 
+import org.junit.jupiter.api.Tag
 import kotlin.test.Test
 
 class AlgorithmsTestsJava : AbstractAlgorithmsTests() {
     @Test
+    @Tag("Easy")
     fun testOptimizeBuyAndSell() {
         optimizeBuyAndSell { JavaAlgorithms.optimizeBuyAndSell(it) }
     }
 
     @Test
+    @Tag("Easy")
     fun testJosephTask() {
         josephTask { menNumber, choiceInterval -> JavaAlgorithms.josephTask(menNumber, choiceInterval) }
     }
 
     @Test
+    @Tag("Normal")
     fun testLongestCommonSubstring() {
         longestCommonSubstring { first, second -> JavaAlgorithms.longestCommonSubstring(first, second) }
     }
 
     @Test
+    @Tag("Easy")
     fun testCalcPrimesNumber() {
         calcPrimesNumber { JavaAlgorithms.calcPrimesNumber(it) }
     }
 
     @Test
+    @Tag("Hard")
     fun testBaldaSearcher() {
         baldaSearcher { inputName, words -> JavaAlgorithms.baldaSearcher(inputName, words) }
     }
--- test/lesson2/AlgorithmsTestsKotlin.kt
+++ test/lesson2/AlgorithmsTestsKotlin.kt
@@ -1,29 +1,35 @@
 package lesson2
 
+import org.junit.jupiter.api.Tag
 import kotlin.test.Test
 
 class AlgorithmsTestsKotlin : AbstractAlgorithmsTests() {
     @Test
+    @Tag("Easy")
     fun testOptimizeBuyAndSell() {
         optimizeBuyAndSell { optimizeBuyAndSell(it) }
     }
 
     @Test
+    @Tag("Easy")
     fun testJosephTask() {
         josephTask { menNumber, choiceInterval -> josephTask(menNumber, choiceInterval) }
     }
 
     @Test
+    @Tag("Normal")
     fun testLongestCommonSubstring() {
         longestCommonSubstring { first, second -> longestCommonSubstring(first, second) }
     }
 
     @Test
+    @Tag("Easy")
     fun testCalcPrimesNumber() {
         calcPrimesNumber { calcPrimesNumber(it) }
     }
 
     @Test
+    @Tag("Hard")
     fun testBaldaSearcher() {
         baldaSearcher { inputName, words -> baldaSearcher(inputName, words) }
     }
