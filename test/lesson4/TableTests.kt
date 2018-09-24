67834186def5c05f499f3ff2ffe686645eea6759
ice-phoenix <Marat.Akhin@gmail.com>
2018-09-24 15:53:30 +0300

    Add @Tag("Example") to the example test in lesson 4

--- test/lesson4/TableTests.kt
+++ test/lesson4/TableTests.kt
@@ -1,11 +1,13 @@
 package lesson4
 
+import org.junit.jupiter.api.Tag
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 // TODO: здесь будут тесты для задач и примеров 4-го урока
 class TableTests {
     @Test
+    @Tag("Example")
     fun testSomeExample() {
         assertEquals(42, someExample())
     }
