0aa56a7a62cd71fc9c2c9767627d4eeaaf85608e
Mikhail Glukhikh <glukhikh@mail.ru>
2018-09-24 12:25:48 +0300

    Lesson 6: fix test longestIncreasingSubSequence

--- test/lesson6/AbstractDynamicTests.kt
+++ test/lesson6/AbstractDynamicTests.kt
@@ -35,7 +35,7 @@ abstract class AbstractDynamicTests {
         assertEquals(listOf(), longestIncreasingSubSequence(listOf()))
         assertEquals(listOf(1), longestIncreasingSubSequence(listOf(1)))
         assertEquals(listOf(1, 2), longestIncreasingSubSequence(listOf(1, 2)))
-        assertEquals(listOf(2, 1), longestIncreasingSubSequence(listOf(2)))
+        assertEquals(listOf(2), longestIncreasingSubSequence(listOf(2, 1)))
         assertEquals(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
                 longestIncreasingSubSequence(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
         )
