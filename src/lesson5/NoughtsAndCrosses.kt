23365fbc37ac6f9167efc5f5226e5101ddadf776
Mikhail Glukhikh <glukhikh@mail.ru>
2018-10-29 14:11:06 +0300

    Noughts & Crosses: AI implementation

--- src/lesson5/NoughtsAndCrosses.kt
+++ src/lesson5/NoughtsAndCrosses.kt
@@ -1,10 +1,14 @@
 package lesson5
 
-class NoughtsAndCrosses {
+class NoughtsAndCrosses() {
 
-    private data class Cell(val x: Int, val y: Int)
+    data class Cell(val x: Int, val y: Int)
 
     private val field = mutableMapOf<Cell, Boolean>()
+
+    constructor(original: NoughtsAndCrosses) : this() {
+        field.putAll(original.field)
+    }
 
     fun makeTurn(x: Int, y: Int, isCross: Boolean): Boolean {
         val cell = Cell(x, y)
@@ -30,5 +34,29 @@ class NoughtsAndCrosses {
             if (line.all { field[it] == false }) return false
         }
         return null
+    }
+
+    fun findBestMoveAndEvaluation(crossesTurn: Boolean, depth: Int = 0): Pair<Cell?, Int> {
+        when (findWinnerIfAny()) {
+            crossesTurn -> return null to 10 - depth
+            !crossesTurn -> return null to -10 + depth
+        }
+        var bestMove: Pair<Cell?, Int> = null to -11
+        for (x in 0..2) {
+            for (y in 0..2) {
+                val move = Cell(x, y)
+                if (move in field) continue
+                val copy = NoughtsAndCrosses(this)
+                copy.makeTurn(x, y, crossesTurn)
+                val evaluation = -copy.findBestMoveAndEvaluation(!crossesTurn, depth + 1).second
+                if (evaluation > bestMove.second) {
+                    bestMove = move to evaluation
+                }
+            }
+        }
+        if (bestMove.first == null) {
+            return null to 0
+        }
+        return bestMove
     }
 }
--- test/lesson5/NoughtsAndCrossesTest.kt
+++ test/lesson5/NoughtsAndCrossesTest.kt
@@ -11,7 +11,9 @@ class NoughtsAndCrossesTest {
         nc.makeTurn(0, 0, true)
         nc.makeTurn(1, 0, false)
         nc.makeTurn(0, 1, true)
+        assertEquals(NoughtsAndCrosses.Cell(0, 2), nc.findBestMoveAndEvaluation(false).first)
         nc.makeTurn(1, 1, false)
+        assertEquals(NoughtsAndCrosses.Cell(0, 2), nc.findBestMoveAndEvaluation(true).first)
         nc.makeTurn(0, 2, true)
         assertEquals(true, nc.findWinnerIfAny())
     }
@@ -22,9 +24,49 @@ class NoughtsAndCrossesTest {
         nc.makeTurn(1, 1, true)
         nc.makeTurn(0, 0, false)
         nc.makeTurn(2, 2, true)
-        nc.makeTurn(2, 0, false)
-        nc.makeTurn(0, 2, true)
-        nc.makeTurn(1, 0, false)
+        assertEquals(NoughtsAndCrosses.Cell(0, 2), nc.findBestMoveAndEvaluation(false).first)
+        nc.makeTurn(0, 2, false)
+        assertEquals(NoughtsAndCrosses.Cell(0, 1), nc.findBestMoveAndEvaluation(true).first)
+        nc.makeTurn(2, 0, true)
+        nc.makeTurn(0, 1, false)
         assertEquals(false, nc.findWinnerIfAny())
+    }
+
+    @Test
+    fun gameThree() {
+        val nc = NoughtsAndCrosses()
+        nc.makeTurn(1, 1, true)
+        nc.makeTurn(0, 0, false)
+        nc.makeTurn(2, 2, true)
+        nc.makeTurn(1, 0, false)
+        assertEquals(NoughtsAndCrosses.Cell(2, 0), nc.findBestMoveAndEvaluation(true).first)
+        nc.makeTurn(2, 0, true)
+        nc.makeTurn(0, 2, false)
+        assertEquals(NoughtsAndCrosses.Cell(2, 1), nc.findBestMoveAndEvaluation(true).first)
+        nc.makeTurn(2, 1, true)
+        assertEquals(true, nc.findWinnerIfAny())
+    }
+
+    @Test
+    fun gameFour() {
+        val nc = NoughtsAndCrosses()
+        nc.makeTurn(1, 1, true)
+        nc.makeTurn(0, 0, false)
+        nc.makeTurn(2, 2, true)
+        nc.makeTurn(1, 0, false)
+        nc.makeTurn(0, 1, true)
+        assertEquals(NoughtsAndCrosses.Cell(2, 0), nc.findBestMoveAndEvaluation(false).first)
+    }
+
+    @Test
+    fun gameFive() {
+        val nc = NoughtsAndCrosses()
+        nc.makeTurn(1, 1, true)
+        nc.makeTurn(0, 0, false)
+        nc.makeTurn(2, 2, true)
+        nc.makeTurn(0, 2, false)
+        assertEquals(0, nc.findBestMoveAndEvaluation(true).second)
+        nc.makeTurn(0, 1, true)
+        assertEquals(0, nc.findBestMoveAndEvaluation(false).second)
     }
 }
