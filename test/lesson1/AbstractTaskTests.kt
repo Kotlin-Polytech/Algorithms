f8b23b07ff2a4de246fbd06c168f8fcf5fcb4076
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2019-09-05 12:59:51 +0300

    reduced number of generated temperatures for sortTemperatures task in lesson1 to solve OOM/timeout problems

--- test/lesson1/AbstractTaskTests.kt
+++ test/lesson1/AbstractTaskTests.kt
@@ -113,7 +113,7 @@ abstract class AbstractTaskTests : AbstractFileTests() {
             }
         }
         testGeneratedTemperatures(10)
-        testGeneratedTemperatures(5000)
+        testGeneratedTemperatures(1000)
     }
 
     protected fun sortSequence(sortSequence: (String, String) -> Unit) {
