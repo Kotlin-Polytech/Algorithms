cc2c25f710314c786ad1c1f14f87fb1acacb7a60
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-09-15 11:03:43 +0300

    Lesson 4: add comments to OpenAddressingSet tasks

--- src/lesson4/OpenAddressingSet.kt
+++ src/lesson4/OpenAddressingSet.kt
@@ -1,5 +1,8 @@
 package lesson4
 
+/**
+ * Множество(таблица) с открытой адресацией на 2^bits элементов без возможности роста.
+ */
 class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>() {
     init {
         require(bits in 2..31)
@@ -11,10 +14,16 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
 
     override var size: Int = 0
 
+    /**
+     * Индекс в таблице, начиная с которого следует искать данный элемент
+     */
     private fun T.startingIndex(): Int {
         return hashCode() and (0x7FFFFFFF shr (31 - bits))
     }
 
+    /**
+     * Проверка, входит ли данный элемент в таблицу
+     */
     override fun contains(element: T): Boolean {
         var index = element.startingIndex()
         var current = storage[index]
@@ -28,6 +37,16 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
         return false
     }
 
+    /**
+     * Добавление элемента в таблицу.
+     *
+     * Не делает ничего и возвращает false, если такой же элемент уже есть в таблице.
+     * В противном случае вставляет элемент в таблицу и возвращает true.
+     *
+     * Бросает исключение (IllegalStateException) в случае переполнения таблицы.
+     * Обычно Set не предполагает ограничения на размер и подобных контрактов,
+     * но в данном случае это было введено для упрощения кода.
+     */
     override fun add(element: T): Boolean {
         val startingIndex = element.startingIndex()
         var index = startingIndex
@@ -46,14 +65,27 @@ class OpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T>(
     }
 
     /**
-     * Для этой задачи пока нет тестов, но вы можете попробовать привести решение и добавить к нему тесты
+     * Удаление элемента из таблицы
+     *
+     * Если элемент есть в таблица, функция удаляет его из дерева и возвращает true.
+     * В ином случае функция оставляет множество нетронутым и возвращает false.
+     * Высота дерева не должна увеличиться в результате удаления.
+     *
+     * Спецификация: {@link Set#remove(Object)} (Ctrl+Click по remove)
+     *
+     * Средняя
      */
     override fun remove(element: T): Boolean {
         TODO("not implemented")
     }
 
     /**
-     * Для этой задачи пока нет тестов, но вы можете попробовать привести решение и добавить к нему тесты
+     * Создание итератора для обхода таблицы
+     *
+     * Не забываем, что итератор должен поддерживать функции next(), hasNext(),
+     * и опционально функцию remove()
+     *
+     * Средняя (сложная, если поддержан и remove тоже)
      */
     override fun iterator(): MutableIterator<T> {
         TODO("not implemented")
